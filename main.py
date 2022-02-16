4db81138b06e369935a4cdfb115413a72ac24e83
Bahaa-Mohammed <98849759+Bahaa-Mohammed@users.noreply.github.com>
2022-02-16 16:27:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from pyrogram import Client, filters
 
-BOT_TOKEN =5245470448:AAHJ0UqCVnuehEREuBqorI9NkzcQVU1K9KU # your bot token from telegram.me/BotFather. Sample :- "12345:abcdefghijklmnop"
-API_ID =17356724 # your api id from my.telegram.org. Sample :- int("123456")
-API_HASH =1c41113550cbfb972d1b95721addf200 # your api hash from my.telegram.org Sample :- "fayasnoushad123"
+BOT_TOKEN = "5245470448:AAHJ0UqCVnuehEREuBqorI9NkzcQVU1K9KU" # your bot token from telegram.me/BotFather. Sample :- "12345:abcdefghijklmnop"
+API_ID = "17356724" # your api id from my.telegram.org. Sample :- int("123456")
+API_HASH = "1c41113550cbfb972d1b95721addf200" # your api hash from my.telegram.org Sample :- "fayasnoushad123"
 
 Bot = Client(
     "Simple-Pyrogram-Bot",
