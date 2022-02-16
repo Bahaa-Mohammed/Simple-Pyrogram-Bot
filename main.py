f8f6148ec35da5f167b713bb1871caf1264999ec
Bahaa-Mohammed <98849759+Bahaa-Mohammed@users.noreply.github.com>
2022-02-16 21:30:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ async def start(bot, update):
 
 @Bot.on_message(filters.command(["f"]) & filters.private)
 async def start(bot, update):
-    await Bot.send_document(message.chat.id,"https://t.me/t7gyyycycuv9v9u/6"
+    await bot.send_document(message.chat.id,"https://t.me/t7gyyycycuv9v9u/6"
                 )
 
 
