e0e40e6164e01d8aca8d8f1a602856d86c085638
Bahaa-Mohammed <98849759+Bahaa-Mohammed@users.noreply.github.com>
2022-02-16 19:23:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -31,5 +31,100 @@ reply_markup=InlineKeyboardMarkup([[
                 [InlineKeyboardButton(
                         "Amazing", url="https://github.com/Achu2234")]]))
 
+START_TEXT = """Hello {},
+I am an under 5MB media or file to telegra.ph link uploader bot.
+
+Made by @FayasNoushad"""
+
+HELP_TEXT = """**About Me**
+
+- Just give me a media under 5MB
+- Then I will download it
+- I will then upload it to the telegra.ph link
+
+Made by @FayasNoushad"""
+
+ABOUT_TEXT = """**About Me**
+
+- **Bot :** `Telegraph Uploader`
+- **Creator :** [Fayas](https://telegram.me/TheFayas)
+- **Channel :** [Fayas Noushad](https://telegram.me/FayasNoushad)
+- **Source :** [Click here](https://github.com/FayasNoushad/Telegraph-Uploader-Bot)
+- **Language :** [Python3](https://python.org)
+- **Library :** [Pyrogram](https://pyrogram.org)"""
+
+START_BUTTONS = InlineKeyboardMarkup(
+    [
+        [
+            InlineKeyboardButton('Help', callback_data='help'),
+            InlineKeyboardButton('About', callback_data='about'),
+            InlineKeyboardButton('Close', callback_data='close')
+        ]
+    ]
+)
+
+HELP_BUTTONS = InlineKeyboardMarkup(
+    [
+        [
+            InlineKeyboardButton('Home', callback_data='home'),
+            InlineKeyboardButton('About', callback_data='about'),
+            InlineKeyboardButton('Close', callback_data='close')
+        ]
+    ]
+)
+
+ABOUT_BUTTONS = InlineKeyboardMarkup(
+    [
+        [
+            InlineKeyboardButton('Channel', url='https://telegram.me/FayasNoushad'),
+            InlineKeyboardButton('Feedback', url='https://telegram.me/TheFayas')
+        ],
+        [
+            InlineKeyboardButton('Home', callback_data='home'),
+            InlineKeyboardButton('Help', callback_data='help'),
+            InlineKeyboardButton('Close', callback_data='close')
+        ]
+    ]
+)
+
+
+@Bot.on_callback_query()
+async def cb_data(bot, update):
+    
+    if update.data == "home":
+        await update.message.edit_text(
+            text=START_TEXT.format(update.from_user.mention),
+            disable_web_page_preview=True,
+            reply_markup=START_BUTTONS
+        )
+    
+    elif update.data == "help":
+        await update.message.edit_text(
+            text=HELP_TEXT,
+            disable_web_page_preview=True,
+            reply_markup=HELP_BUTTONS
+        )
+    
+    elif update.data == "about":
+        await update.message.edit_text(
+            text=ABOUT_TEXT,
+            disable_web_page_preview=True,
+            reply_markup=ABOUT_BUTTONS
+        )
+    
+    else:
+        await update.message.delete()
+    
+
+@Bot.on_message(filters.private & filters.command(["start"]))
+async def start(bot, update):
+    
+    await update.reply_text(
+        text=START_TEXT.format(update.from_user.mention),
+        disable_web_page_preview=True,
+        quote=True,
+        reply_markup=START_BUTTONS
+    )
+
 
 Bot.run()
