a877e4bf29f7a16e4441a3474f09436e71f16d6e
Bahaa-Mohammed <98849759+Bahaa-Mohammed@users.noreply.github.com>
2022-03-02 21:48:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -136,26 +136,5 @@ async def start(bot, update):
     )
 
 
-@Bot.on_inline_query()
-async def start(bot, inline_query):
-    inline_query.answer(
-        results=[
-            InlineQueryResultArticle(
-                title="Installation",
-                input_message_content=InputTextMessageContent(
-                    "Here's how to install Pyrogram"
-                ),
-                url="https://docs.pyrogram.org/intro/install",
-                description="How to install Pyrogram",
-                thumb_url="https://i.imgur.com/JyxrStE.png",
-                reply_markup=InlineKeyboardMarkup(
-                    [
-                        [InlineKeyboardButton(
-                            "Open website",
-                            url="https://docs.pyrogram.org/intro/install"
-                        )]
-                    ]
-                )
-            )
 
 Bot.run()
