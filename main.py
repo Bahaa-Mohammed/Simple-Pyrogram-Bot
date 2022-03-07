e9c33453790d5ea0550fd9a0f01e418edf61183c
Bahaa-Mohammed <98849759+Bahaa-Mohammed@users.noreply.github.com>
2022-03-07 18:24:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 import asyncio
 
-BOT_TOKEN = "5245470448:AAHJ0UqCVnuehEREuBqorI9NkzcQVU1K9KU" # your bot token from telegram.me/BotFather. Sample :- "12345:abcdefghijklmnop"
-API_ID = "17356724" # your api id from my.telegram.org. Sample :- int("123456")
-API_HASH = "1c41113550cbfb972d1b95721addf200" # your api hash from my.telegram.org Sample :- "fayasnoushad123"
+BOT_TOKEN = "" # your bot token from telegram.me/BotFather. Sample :- "12345:abcdefghijklmnop"
+API_ID = "" # your api id from my.telegram.org. Sample :- int("123456")
+API_HASH = "" # your api hash from my.telegram.org Sample :- "fayasnoushad123"
 
 Bot = Client(
     "Simple-Pyrogram-Bot",
