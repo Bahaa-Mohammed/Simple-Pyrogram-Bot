985bfd11ba4031f7c472870d336cf46d9c59cdb0
Bahaa-Mohammed <98849759+Bahaa-Mohammed@users.noreply.github.com>
2022-04-03 06:18:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -112,13 +112,13 @@ async def start(bot, update):
             reply_markup=HELP_BUTTONS
         )
     
-    else: update.data == "about":
+    elif update.data == "about":
         await update.message.edit_text(
             text=ABOUT_TEXT,
             disable_web_page_preview=True,
             reply_markup=ABOUT_BUTTONS
         )
-    
+    else: await update.message.delete()
     
 
 @Bot.on_message(filters.command(["startt"]))
